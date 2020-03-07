2521086ff89da0eb217feabf8813cb4eb968e29f
Frank Schmidt <frank.schmidt@codecentric.de>
2020-03-07 23:47:25 +0100

    Crude solve of s1c4, have to improve scoring though

---
+++ src/bin/s1c4.rs
@@ -0,0 +1,23 @@
+use std::fs;
+
+use cyptopals::decrypt_xor_single_byte;
+
+/// Find the ciphertext that is probably encrypted with single byte xor
+fn main() {
+    let line = fs::read_to_string("./files/find_single_byte_xor.txt").unwrap();
+    let lines: Vec<Vec<u8>> = line.split("\n")
+        .filter(|x| x.len() > 2)
+        .map(|x| x.trim())
+        .map(|x| hex::decode(x).unwrap())
+        .collect();
+    let mut best_decryptions = Vec::new();
+    for cipher in lines {
+        let possible_best = decrypt_xor_single_byte(cipher);
+        best_decryptions.push(possible_best.first().unwrap().clone());
+    }
+    best_decryptions.sort_by_key(|t| -t.0);
+    for i in 0..5 {
+        let (score, key, text) = best_decryptions[i].clone();
+        println!("{}, {}: {:?}", score, key, String::from_utf8(text));
+    }
+}
