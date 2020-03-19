c7bc502010adcd77fd433e71ca8308c766a46691
Frank Schmidt <frank.schmidt@codecentric.de>
2020-03-19 23:25:10 +0100

    s2c14: byte at a time decryption with random prefix

---
+++ src/bin/s2c14.rs
@@ -0,0 +1,102 @@
+extern crate rand;
+extern crate log;
+extern crate env_logger;
+
+use std::collections::HashSet;
+
+use cyptopals::{aes_ecb_encrypt, compare, detect_blocksize, detect_ecb, pad_pkcs7, random_128_bit, extract_fixed_suffix};
+
+/// Find a block in a ciphertext and return the starting offset
+fn find_block(cipher: &Vec<u8>, block: &Vec<u8>) -> Option<usize> {
+    let blocksize = block.len();
+    'blocks: for i in 0..(cipher.len() / blocksize) {
+        for j in 0..blocksize {
+            if cipher[i * blocksize + j] != block[j] {
+                continue 'blocks;
+            }
+        }
+        return Some(i * blocksize);
+    }
+    None
+}
+
+#[test]
+fn test_find_block() {
+    assert_eq!(find_block(&vec![1, 2, 3, 4], &vec![2, 3]), None);
+    assert_eq!(find_block(&vec![1, 2, 3, 4], &vec![3, 4]), Some(2usize));
+}
+
+fn find_first_duplicate_block(cipher: &Vec<u8>) -> Option<Vec<u8>> {
+    let mut blocks = HashSet::new();
+    for i in (0..cipher.len()).step_by(16) {
+        let block = cipher.get(i..i + 16);
+        match block {
+            Some(b) => {
+                if blocks.contains(&b) { return Some(b.to_vec()); };
+                blocks.insert(b);
+            }
+            None => {}
+        }
+    }
+    None
+}
+
+
+/// Byte at a time ecb decryption with a random (length and content) prefix
+fn main() {
+    env_logger::init();
+
+    let secret_data = base64::decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK").unwrap();
+    let key = random_128_bit();
+    let mut encrypt_blackbox = |plain: Vec<u8>| {
+        // random length random prefix
+        let mut plain_mod = random_128_bit();
+        plain_mod.extend(random_128_bit());
+        let to_drop: u8 = rand::random();
+        for _ in 0..(to_drop % 31) {
+            plain_mod.pop();
+        }
+        plain_mod.extend(plain);
+        plain_mod.extend_from_slice(secret_data.as_slice());
+        aes_ecb_encrypt(pad_pkcs7(plain_mod, 16), &key)
+    };
+
+    let blocksize = detect_blocksize(&mut encrypt_blackbox).unwrap();
+    println!("Cipher has blocksize {} bytes", blocksize);
+    println!("Cipher is using ecb: {}", detect_ecb(&mut encrypt_blackbox, blocksize));
+
+    // Use longer pattern to reduce probability of accidental matches in wrapped_blackbox
+    // if we use AAAA, any random A at the end of the prefix might trick our detection later
+    let plain_known = vec![0x41,0x42,0x43,0x44].repeat(4);
+    let mut known_block = find_first_duplicate_block(&encrypt_blackbox(plain_known.repeat(2)));
+    while known_block.is_none() {
+        known_block = find_first_duplicate_block(&encrypt_blackbox(plain_known.repeat(2)));
+    }
+    let known_block = known_block.unwrap();
+    println!("found marker block");
+
+
+    // retry encryption (1/16 chance the random prefix fits) until we find the `known_block`.
+    // We remove everything up to and including that block.
+    // This way, the problem is reduced to the easy case, no unknown prefix left.
+    let mut wrapped_blackbox = |plain: Vec<u8>| {
+        loop {
+            let mut plain_mut = plain_known.clone();
+            plain_mut.extend(&plain);
+            let mut potential_cipher = encrypt_blackbox(plain_mut);
+            let position = find_block(&potential_cipher, &known_block);
+            if position.is_some() {
+                let cipher = potential_cipher.split_off(position.unwrap() + blocksize);
+                // checking that removal works correctly
+//                println!("removed {:?}", potential_cipher);
+//                println!("known   {:?}", known_block);
+//                println!("ret {:?}", cipher);
+                return cipher;
+            }
+        }
+    };
+
+    let decrypted_secret = extract_fixed_suffix(&mut wrapped_blackbox, blocksize);
+    println!("decrypted secret matches: {} (len: {}, {})", decrypted_secret == secret_data, decrypted_secret.len(), secret_data.len());
+    assert!(compare(&decrypted_secret, &secret_data));
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate base64;
 extern crate hex;
+#[macro_use]
+extern crate log;
 
 use std::collections::HashSet;
 
@@ -134,6 +136,48 @@ pub fn break_xor_repeating_key_size(cipher: &Vec<u8>, keysize: usize) -> Vec<(Sc
     let key: Vec<u8> = best_key.iter().map(|t| t.1).collect();
     let plain = xor_repeating_key(cipher.clone(), key.clone());
     vec![(rate_plain(&plain), key, plain)]
+}
+
+/// Extract an appended secret from the encryption blackbox
+///
+/// Requires the blackbox to have the following properties:
+///   - blackbox(text) = enc(text||secret)
+///   - blackbox(text) stays the same for consecutive calls
+///   - blackbox uses pkcs7 padding for the plaintext (for the stopping condition)
+pub fn extract_fixed_suffix(blackbox: &mut dyn Fn(Vec<u8>) -> Vec<u8>, blocksize: usize) -> Vec<u8> {
+    let secret_len = blackbox(vec![]).len();
+    let mut found = Vec::new();
+    for i in 0..secret_len {
+        let mut prefix = vec![31; blocksize - 1 - (i % blocksize)];
+        let to_match = blackbox(prefix.clone());
+        prefix.extend(&found);
+        let found_len = found.len();
+        let found_blocks = found.len() / blocksize;
+        for c in 0..255u8 {
+            prefix.push(c);
+            // compare only useful block = the one containing the border between prefix and secret
+            // previous blocks are equal due to construction, blocks after contain an additional copy of secret
+            let working_block = (found_blocks * 16)..((found_blocks + 1) * 16);
+            if blackbox(prefix.clone()).get(working_block.clone()).unwrap() == to_match.get(working_block).unwrap() {
+                info!("@{} found: {}", i, c);
+                found.push(c);
+                break;
+            }
+            prefix.pop();
+        }
+        // if we have some chars left, but could not find a match, it is probably because we are trying to decode padding
+        // the padding changes though: Let's assume the padding is '\x02\x02'
+        // we first try to find the first char, with a prefix so that it is the last char before a new block.
+        // Therefore the padding changes to '\x01' which we find.
+        // This leaves us with '\x01\x??' whose ciphertext can never match a plaintext ending in '\x02\x02'.
+        debug!("intermediate: {:?}", found);
+        if found.len() == found_len {
+            info!("Found {} - 1 secret bytes", found.len());
+            assert_eq!(found.pop().unwrap(), 1u8, "If this uses pkcs7 padding, we expect a 1");
+            break;
+        }
+    }
+    found
 }
 
 // UTIL
