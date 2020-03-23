3db6d7d18f0549a4971034ad479d654daaee3252
Frank Schmidt <frank.schmidt@codecentric.de>
2020-03-23 21:21:17 +0100

    s3c20: Well this is awkward...

---
+++ src/bin/s3c20.rs
@@ -0,0 +1,6 @@
+/// # Decrypt CTR with static nonce (take 2)
+/// Well, I already chose to solve the challenge 'the better way' by instinct.
+/// Soo, this is a noop.
+fn main() {
+    println!("See c319, kthxbye");
+}
