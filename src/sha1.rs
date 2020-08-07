b639e0f161a29f58f1ec4bec4bac2e29bb1f3a87
Frank Schmidt <frank.schmidt@codecentric.de>
2020-08-07 15:13:05 +0200

    Write my own sha1 implementation
    adopting pseudocode on wikipedia worked almost perfectly.
    but I did not understand the behavior of the different binary operators.
    largest timesink: '<<' does not wrap, but rotate_left does.
    Luckily I found a document by NIST that has intermediate results:
    https://csrc.nist.gov/projects/cryptographic-standards-and-guidelines/example-values

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,182 +1,199 @@
 //! # Implement SHA1, trying to proxy the std one first
-use core::fmt;
-use std::fmt::{Debug, Formatter};
-use std::mem;
+//! learned: <<1 != rotate_left -> wrapping vs not
 
-use crypto::digest::Digest;
-use crypto::sha1;
+const STARTING_STATE: [u32; 5] = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
 
-/// from crypto::sha1::Sha1
-#[derive(Debug)]
-pub struct HackSha1 {
-    h: [u32; 5],
-    _length_bits: u64,
-    _buffer: FixedBuffer64,
-    _computed: bool,
-}
+pub struct MySha1 { h: [u32; 5] }
 
-struct FixedBuffer64 {
-    _buffer: [u8; 64],
-    _buffer_idx: usize,
-}
-
-impl Debug for FixedBuffer64 {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let mut s = f.debug_struct("Buffer");
-        s.field("idx", &self._buffer_idx);
-        if self._buffer_idx > 0 { s.field("last", &self._buffer[self._buffer_idx - 1]); }
-        s.finish()
+impl MySha1 {
+    /// Obtain only the padding for a certain input length.
+    /// This code assumes, that len is the number of bytes and data is byte aligned.
+    pub fn padding(len: usize) -> Vec<u8> {
+        let mut padding = vec![0x80];
+        let mut len_bits = len * 8;
+        // might be off by one
+        if len_bits % 512 > 448 {
+            len_bits += 72;
+            padding.append(&mut vec![0; 9]);
+        }
+        let zeros = 512 - (len_bits % 512) - 72;
+        println!("add {} zero bits: {}", zeros, zeros / 8);
+        for _ in 0..zeros / 8 {
+            padding.push(0 as u8);
+        }
+        padding.append(&mut ((len * 8) as u64).to_be_bytes().to_vec());
+        padding
     }
-}
-
-pub struct Sha1 { sha1: sha1::Sha1 }
-
-impl Sha1 {
-    pub fn new() -> Sha1 {
-        Sha1 { sha1: sha1::Sha1::new() }
+    pub fn pad(mut input: Vec<u8>) -> Vec<u8> {
+        input.append(&mut MySha1::padding(input.len()));
+        input
     }
 
-    /// Based on assumptions about the rust crypto implementation, we can get access to the underlying state
-    /// Prone to breaking if [sha1::Sha1] struct changes
-    pub fn new_with_state(state: [u32; 5]) -> Sha1 {
-        let sha1 = sha1::Sha1::new();
-        // this works as long as the underlying structure does not change
-        // otherwise i expect this to break horribly
-        let mut hacked: HackSha1 = unsafe { mem::transmute(sha1) };
-        hacked.h = state;
-        let sha1_changed = unsafe { mem::transmute(hacked) };
-        Sha1 { sha1: sha1_changed }
+    /// Hash input vector, assumes padding has been applied already
+    pub fn hash(input: Vec<u8>) -> [u32; 5] {
+        MySha1::hash_with_initial_state(STARTING_STATE, input)
     }
 
-    pub fn set_state(&mut self, state: [u32; 5]) {
-        // this works as long as the underlying structure does not change
-        // otherwise i expect this to break horribly
-        let mut hacked: HackSha1 = unsafe { mem::transmute(self.sha1) };
-        println!("before {:?}", hacked);
-        hacked.h = state;
-        println!("after {:?}", hacked);
-        let sha1_changed = unsafe { mem::transmute(hacked) };
-        self.sha1 = sha1_changed;
-    }
-
-
-    /// Based on assumptions about the rust crypto implementation, we can get access to the state
-    pub fn get_state(&mut self) -> [u32; 5] {
-        // this works as long as the underlying structure does not change
-        // otherwise i expect this to break horribly
-        let hacked: HackSha1 = unsafe { mem::transmute(self.sha1) };
-        println!("{:?}", hacked);
-        hacked.h
-    }
-
-    pub fn result_vec(&mut self) -> Vec<u8> {
-        let mut out = vec![0u8; self.sha1.output_bytes()];
-        self.sha1.result(&mut out);
-        out.to_vec()
+    /// Hash input vector with a specific starting state, assumes padding has been applied already
+    pub fn hash_with_initial_state(state: [u32; 5], input: Vec<u8>) -> [u32; 5] {
+        assert_eq!(input.len() % (512 / 8), 0, "input length has to be a multiple of 512 bits");
+        let mut sha1 = MySha1 { h: state };
+        for i in (0..input.len()).step_by(64) {
+            let mut block: [u8; 64] = [0; 64];
+            block.copy_from_slice(&input[i..i + 64]);
+            do_block(&mut sha1, block);
+        }
+        sha1.h
     }
 
     /// Perform Sha1(key||data)
-    pub fn keyed_mac(key: &Vec<u8>, data: &Vec<u8>) -> Vec<u8> {
-        let mut sha1 = Sha1::new();
-        sha1.input(key);
-        sha1.input(data);
-        let mut out = vec![0u8; sha1.output_bytes()];
-        sha1.result(&mut out);
-        sha1.get_state();
-        out
+    pub fn keyed_mac(key: &Vec<u8>, data: &Vec<u8>) -> [u32; 5] {
+        let mut input = key.clone();
+        input.append(&mut data.clone());
+
+        MySha1::hash(input)
     }
 
     /// validate that Sha1(key||data) == mac
-    pub fn validate_mac(key: &Vec<u8>, data: &Vec<u8>, mac: &Vec<u8>) -> bool {
-        let mut sha1 = Sha1::new();
-        sha1.input(&key);
-        sha1.input(&data);
-        let mut out = vec![0u8; sha1.output_bytes()];
-        sha1.result(&mut out);
-        out == *mac
+    pub fn validate_mac(key: &Vec<u8>, data: &Vec<u8>, mac: &[u32; 5]) -> bool {
+        let mut input = key.clone();
+        input.append(&mut data.clone());
+        MySha1::hash(input) == *mac
     }
 }
 
+fn do_block(sha1: &mut MySha1, block: [u8; 64]) {
+    let mut w = [0u32; 80];
+    // fancy self written byte to u32 converter
+    for i in 0..64 {
+        let tmp: u32 = (block[i] as u32).rotate_left(8 * (3 - i % 4) as u32);
+        w[i / 4] = w[i / 4] | tmp;
+    }
 
-impl Digest for Sha1 {
-    fn input(&mut self, input: &[u8]) { self.sha1.input(input) }
+    // prepare and expand
+    for i in 16..80 {
+        w[i] = (w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16]).rotate_left(1);
+    }
+    println!("{:x?}", block[0..64].to_vec());
+    println!("{:x?}", w[0..16].to_vec());
 
-    fn result(&mut self, out: &mut [u8]) { self.sha1.result(out) }
+    let mut a = sha1.h[0];
+    let mut b = sha1.h[1];
+    let mut c = sha1.h[2];
+    let mut d = sha1.h[3];
+    let mut e = sha1.h[4];
 
-    fn reset(&mut self) { self.sha1.reset(); }
+    for i in 0..80 {
+        let mut f = 0;
+        let mut k = 0;
+        if i <= 19 {
+            f = (b & c) | ((!b) & d);
+            k = 0x5A827999;
+            println!("f,k: {:0x} {:0x}", f, k)
+        } else if 20 <= i && i <= 39 {
+            f = b ^ c ^ d;
+            k = 0x6ED9EBA1;
+        } else if 40 <= i && i <= 59 {
+            f = (b & c) | (b & d) | (c & d);
+            k = 0x8F1BBCDC;
+        } else if 60 <= i && i <= 79 {
+            f = b ^ c ^ d;
+            k = 0xCA62C1D6;
+        }
 
-    fn output_bits(&self) -> usize { self.sha1.output_bits() }
+        let tmp = (a.rotate_left(5)).wrapping_add(f).wrapping_add(e).wrapping_add(k).wrapping_add(w[i]);
+        e = d;
+        d = c;
+        c = b.rotate_left(30);
+        b = a;
+        a = tmp;
+        println!("{}: {:08x?} {:08x?} {:08x?} {:08x?} {:08x?}", i, a, b, c, d, e);
 
-    fn block_size(&self) -> usize { self.sha1.block_size() }
+        // if i == 0 {
+        //     assert_eq!([0x0116FC33, 0x67452301, 0x7BF36AE2, 0x98BADCFE, 0x10325476], [a, b, c, d, e])
+        // } else if i == 10 {
+        //     assert_eq!([0xFBDA9E89, 0x8351F929, 0x12DAB8CA, 0x27A301F5, 0x664F8C30], [a, b, c, d, e])
+        // } else if i == 15 {
+        //     assert_eq!([0x20BDD62F, 0x196BEE77, 0x644A3DA5, 0x1181ED99, 0x18C623F9], [a, b, c, d, e])
+        // } else if i == 17 {
+        //     assert_eq!([0x82AA6728, 0x4E925823, 0xC82F758B, 0xC65AFB9D, 0x644A3DA5], [a, b, c, d, e])
+        // } else if i == 18 {
+        //     assert_eq!([0xDC64901D, 0x82AA6728, 0xD3A49608, 0xC82F758B, 0xC65AFB9D], [a, b, c, d, e])
+        // } else if i == 19 {
+        //     assert_eq!([0xFD9E1D7D, 0xDC64901D, 0x20AA99CA, 0xD3A49608, 0xC82F758B], [a, b, c, d, e])
+        // } else if i == 20 {
+        //     assert_eq!([0x1A37B0CA, 0xFD9E1D7D, 0x77192407, 0x20AA99CA, 0xD3A49608], [a, b, c, d, e])
+        // } else if i == 30 {
+        //     assert_eq!([0x1267B407, 0xC5FBAF5D, 0xA0DC2D4B, 0xD2AA1365, 0x6F8D7EF5], [a, b, c, d, e])
+        // } else if i == 50 {
+        //     assert_eq!([0x641DB2CE, 0x120731C5, 0xEEBCE4CD, 0xAFAB40B2, 0x2806E11B], [a, b, c, d, e])
+        // } else if i == 70 {
+        //     assert_eq!([0xC199F8C7, 0x3F2588C2, 0x125C24F0, 0xDE37534A, 0x030F7CAD], [a, b, c, d, e])
+        // }
+    }
+
+    sha1.h[0] = sha1.h[0].wrapping_add(a);
+    sha1.h[1] = sha1.h[1].wrapping_add(b);
+    sha1.h[2] = sha1.h[2].wrapping_add(c);
+    sha1.h[3] = sha1.h[3].wrapping_add(d);
+    sha1.h[4] = sha1.h[4].wrapping_add(e);
+    println!("{:x?}", sha1.h);
 }
 
-/// Obtain only the padding for a certain input length.
-/// This code assumes, that len is the number of bytes and data is byte aligned.
-pub fn padding(len: usize) -> Vec<u8> {
-    let mut padding = vec![0x80];
-    let mut len_bits = len * 8;
-    // might be off by one
-    if len_bits % 512 > 448 {
-        len_bits += 72;
-        padding.append(&mut vec![0; 9]);
-    }
-    let zeros = 512 - (len_bits % 512) - 72;
-    println!("add {} zero bits: {}", zeros, zeros / 8);
-    for _ in 0..zeros / 8 {
-        padding.push(0 as u8);
-    }
-    padding.append(&mut ((len * 8) as u64).to_be_bytes().to_vec());
-    padding
-}
 
+// impl Digest for Sha1 {
+//     fn input(&mut self, input: &[u8]) { self.sha1.input(input) }
+//
+//     fn result(&mut self, out: &mut [u8]) { self.sha1.result(out) }
+//
+//     fn reset(&mut self) { self.sha1.reset(); }
+//
+//     fn output_bits(&self) -> usize { self.sha1.output_bits() }
+//
+//     fn block_size(&self) -> usize { self.sha1.block_size() }
+// }
+
+/// Write a u32 into a vector, which must be 4 bytes long. The value is written in big-endian
+/// format. (from crypto:cryptoutils
+// pub fn write_u32_be(dst: &mut [u8], mut input: u32) {
+//     assert!(dst.len() == 4);
+//     input = input.to_be();
+//     unsafe {
+//         let tmp = &input as *const _ as *const u8;
+//         ptr::copy_nonoverlapping(tmp, dst.get_unchecked_mut(0), 4);
+//     }
+// }
 #[cfg(test)]
 mod tests {
+    use crypto::digest::Digest;
+    use crypto::sha1::Sha1;
+
     use super::*;
 
     #[test]
     fn sha1_working_correctly() {
-        let mut hash = Sha1::new();
-        hash.input(&[]);
+        let hash = MySha1::hash(MySha1::pad(b"abc".to_vec()));
+        let expected = [0xa9993e36, 0x4706816a, 0xba3e2571, 0x7850c26c, 0x9cd0d89d];
+        println!("ex: {:0x?}", expected);
+        println!("is: {:0x?}", hash);
+        assert_eq!(hash, expected);
 
-        assert_eq!(hash.result_str(), "da39a3ee5e6b4b0d3255bfef95601890afd80709");
-    }
-
-    #[test]
-    fn sha1_unsafe_code_not_obviously_broken() {
-        let mut hash_hacked = Sha1::new_with_state([0, 1, 2, 3, 4]);
-        assert_eq!(hash_hacked.get_state(), [0, 1, 2, 3, 4]);
-        hash_hacked.input(&[2; 50]);
-        assert_eq!(hash_hacked.result_str(),
-                   "c3076928b640fff2b352d65a7b3c380170baffe7", // calculated correctly
-                   "changed the whole state, depends on sha1::Sha1 memory layout, \
-                       error means the HackedSha1 struct should be checked");
-
-        //using the default values should produce the one for the empty string
-        let default_start_state = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
-        let mut hash_hacked = Sha1::new_with_state(default_start_state);
-        assert_eq!(hash_hacked.result_str(), "da39a3ee5e6b4b0d3255bfef95601890afd80709", "using default state");
-    }
-
-    #[test]
-    fn overriding_the_state_produces_the_same_result() {
-        // requirement: strings have to be >64 chars long (internal buffer) and have the same length
-        let mut sha1 = Sha1::new();
-        sha1.input(b"ab---------------------------------------------------------------------------------------------------------------------------");
-        let mut sha2 = Sha1::new();
-        sha2.input(b"00---------------------------------------------------------------------------------------------------------------------------");
-        sha2.set_state(sha1.get_state());
-        assert_eq!(sha1.result_vec(), sha2.result_vec())
+        let hash = MySha1::hash(MySha1::pad(b"".to_vec()));
+        let expected: [u32; 5] = [0xda39a3ee, 0x5e6b4b0d, 0x3255bfef, 0x95601890, 0xafd80709];
+        println!("ex: {:0x?}", expected.to_vec());
+        println!("is: {:0x?}", hash.to_vec());
+        assert_eq!(hash, expected);
     }
 
     #[test]
     fn correct_padding() {
-        let pad = padding(1);
+        let pad = MySha1::padding(1);
         assert_eq!(pad[0], 0x80, "first padding byte");
         assert_eq!(*pad.last().unwrap(), 8, "last padding byte");
         assert_eq!(pad.len(), 64 - 1, "length");
 
         // additional block necessary
-        let pad = padding(57);
+        let pad = MySha1::padding(57);
         assert_eq!(pad[0], 0x80, "first padding byte");
         assert_eq!(*pad.last().unwrap(), (57 * 8) as u8, "last padding byte");
         assert_eq!(pad.len(), 64 * 2 - 57, "length");
