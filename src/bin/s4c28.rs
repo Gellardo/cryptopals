4141656f53857d94c53ddb782982d2ff4a2e262d
Frank Schmidt <frank.schmidt@codecentric.de>
2020-08-28 08:55:45 +0200

    Fix build errors

--- src/bin/s4c28.rs
+++ src/bin/s4c28.rs
@@ -1,9 +1,9 @@
 //! # Rust SHA1 implementation
-use cyptopals::sha1::Sha1;
+use cyptopals::sha1::MySha1;
 use crypto::digest::Digest;
+use cyptopals::u32_be_bytes;
 
 fn main() {
-    let mut sha1 = Sha1::new();
-    sha1.input(&[44;5]);
-    println!("Sha1 works: {}", sha1.result_str());
+    let mut sha1 = MySha1::hash(vec![44;5]);
+    println!("Sha1 works: {}", hex::encode(u32_be_bytes(&sha1)));
 }
