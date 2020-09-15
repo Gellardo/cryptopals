a5a28b8c48d3be1cd23066ae35e3868cd9f382b8
Frank Schmidt <frank.schmidt@codecentric.de>
2020-09-15 22:13:06 +0200

    c34: Actually do what the challenge says and fix the key

--- src/bin/s5c34.rs
+++ src/bin/s5c34.rs
@@ -5,6 +5,14 @@
 //! Now he only has to act as a proxy:
 //! 1. decrypt the messages with the correct key when receiving
 //! 2. re-encrypt them with the other key before forwarding them to the other side.
+//!
+//! Or read the challenge correctly and replace A/B in the messages with p.
+//! That results in the following computation for the shared secret value:
+//! ```
+//! A^b % p = p^b % p = 0
+//! B^a % p = p^a % p = 0
+//! ```
+//! Therefore the generated secret Key will always be the same value.
 
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
@@ -36,6 +44,10 @@ enum Messages {
     },
 }
 
+fn gen_key(value: BigUint) -> Vec<u8> {
+    u32_be_bytes(&MySha1::hash(MySha1::pad(value.to_bytes_be()))[0..4])
+}
+
 fn encrypt(plain: Vec<u8>, key: &Vec<u8>, iv: &Vec<u8>) -> Vec<u8> {
     aes_cbc_encrypt(&pad_pkcs7(plain, 16), key, iv)
 }
@@ -60,40 +72,36 @@ fn main() {
     let (rx_alice, rx_m_alice) = (rx_m_alice, rx_alice);
 
     let malroy = thread::spawn(move || {
-        let (dh, s_a) = match rx_m_bot.recv().unwrap() {
-            Messages::DhSetup { p, g, a_pub } => {
-                let dh = begin_dh(p, g);
-                let s_a = dh.gen_shared(&a_pub);
-                (dh, s_a)
-            }
+        let dh = match rx_m_bot.recv().unwrap() {
+            Messages::DhSetup { p, g, a_pub } => begin_dh(p, g),
             _ => panic!("never"),
         };
         tx_m_bot
             .send(DhSetup {
                 p: dh.p.clone(),
                 g: dh.g.clone(),
-                a_pub: dh.gen_pub(),
+                a_pub: dh.p.clone(),
             })
             .unwrap();
         tx_m_alice
             .send(DhFinish {
-                b_pub: dh.gen_pub(),
+                b_pub: dh.p.clone(),
             })
             .unwrap();
 
-        let s_b = match rx_m_alice.recv().unwrap() {
+        // result for alice and bot will always be 0
+        let s = gen_key(BigUint::from(0u8));
+        println!("mallroy secret = {:?}", s);
+
+        match rx_m_alice.recv().unwrap() {
             DhFinish { b_pub } => dh.gen_shared(&b_pub),
             _ => panic!("never"),
         };
-        let s_a = u32_be_bytes(&MySha1::hash(s_a.to_bytes_be())[0..4]);
-        let s_b = u32_be_bytes(&MySha1::hash(s_b.to_bytes_be())[0..4]);
-        println!("m: s_alice={:?}", s_a);
-        println!("m: s_bot  ={:?}", s_b);
         //DH finished
 
         let message_alice = match rx_m_bot.recv().unwrap() {
             Messages::EncryptedMessage { cipher, iv } => {
-                let plain = decrypt(&cipher, &s_a, &iv);
+                let plain = decrypt(&cipher, &s, &iv);
                 plain
             }
             _ => panic!("never"),
@@ -102,14 +110,14 @@ fn main() {
         let iv = random_128_bit();
         tx_m_bot
             .send(EncryptedMessage {
-                cipher: encrypt(message_alice, &s_b, &iv),
+                cipher: encrypt(message_alice, &s, &iv),
                 iv,
             })
             .unwrap();
 
         let message_bot = match rx_m_alice.recv().unwrap() {
             Messages::EncryptedMessage { cipher, iv } => {
-                let plain = decrypt(&cipher, &s_b, &iv);
+                let plain = decrypt(&cipher, &s, &iv);
                 plain
             }
             _ => panic!("never"),
@@ -118,7 +126,7 @@ fn main() {
         let iv = random_128_bit();
         tx_m_alice
             .send(EncryptedMessage {
-                cipher: encrypt(message_bot, &s_a, &iv),
+                cipher: encrypt(message_bot, &s, &iv),
                 iv,
             })
             .unwrap();
@@ -140,8 +148,7 @@ fn main() {
             Messages::DhFinish { b_pub } => dh.gen_shared(&b_pub),
             _ => panic!("oh no dh bot"),
         };
-        let secret = MySha1::hash(secret.to_bytes_be());
-        let secret = u32_be_bytes(&secret[0..4]);
+        let secret = gen_key(secret);
         println!("alice secret = {:?}", secret);
 
         let iv = random_128_bit();
@@ -166,8 +173,7 @@ fn main() {
             }
             _ => panic!("oh no dh alice"),
         };
-        let secret = MySha1::hash(secret.to_bytes_be());
-        let secret = u32_be_bytes(&secret[0..4]);
+        let secret = gen_key(secret);
         println!("bot secret = {:?}", secret);
 
         tx.send(DhFinish { b_pub }).unwrap();
